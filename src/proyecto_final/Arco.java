d9b56580375910faa10ecefebbfb73d102299769
nico <nicoabgv@gmail.com>
2023-11-18 23:38:13 +0100

    Arco.java actualizado

--- src/proyecto_final/Arco.java
+++ src/proyecto_final/Arco.java
@@ -1,5 +1,20 @@
 package proyecto_final;
 
-public class Arco {
+public class Arco extends Herramienta{
+
+	private double multiplicadorDano;
+	
+	public Arco(int id, int danoBase, double multiplicadorDano) {
+		super(id,danoBase);
+		this.multiplicadorDano = multiplicadorDano;
+    }
+	
+	public double getMultiplicadorDano() {
+        return multiplicadorDano;
+    }
+
+    public void setMultiplicadorDano(double multiplicadorDano) {
+        this.multiplicadorDano = multiplicadorDano;
+    }
 
 }
